722b0426a7cdac0fc61d0dd49ffe5f73f9417430
Torstein Bjørnstad <torstein.bjornstad@nrk.no>
2017-06-29 15:32:17 +0200

    Output data link when posting to slack

--- src/api/events/index.js
+++ src/api/events/index.js
@@ -1,18 +1,26 @@
 const express = require('express');
 
-module.exports = ({slack, eventRepository}) => {
+
+module.exports = ({config, slack, eventRepository}) => {
   const router = new express.Router();
+  const getURLPrefix = req => {
+    const port = (config.port === 80 || config.port === 443) ? '' : `:${config.port}`;
+    return `${req.protocol}://${req.hostname}${port}`;
+  };
 
   router.post('/', (req, res) => {
+    const urlPrefix = getURLPrefix(req);
     const data = req.body;
     const {owner, jobId} = data;
-    slack.send(`Mottok jobb for ${owner}: ${JSON.stringify(data)}`);
+    const jobURL = `${urlPrefix}/jobs/${jobId}`;
+
+    slack.send(`Mottok jobb for ${owner}: ${jobURL}`);
     eventRepository.add(jobId, owner, data)
       .then(() => {
         res.send({
           links: {
-            job: `/jobs/${jobId}`,
-            userJobs: `/users/${owner}/jobs`
+            job: jobURL,
+            userJobs: `${urlPrefix}/users/${owner}/jobs`
           },
           received: data
         });
